5d3b088c9d38f96e6a587f1fb706ba3e619b48f0
Adrixn23 <AdrixnDeveloper29@gmail.com>
2026-04-21 03:11:04 -0400

    Actualizacion

---
+++ Script_BaseDeDatos.sql
@@ -0,0 +1,53 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260421055732_InitialCreate'
+)
+BEGIN
+    CREATE TABLE [Countries] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NOT NULL,
+        [Code] nvarchar(max) NOT NULL,
+        [BaseShippingRate] decimal(18,2) NOT NULL,
+        [IsActive] bit NOT NULL,
+        CONSTRAINT [PK_Countries] PRIMARY KEY ([Id])
+    );
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260421055732_InitialCreate'
+)
+BEGIN
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'BaseShippingRate', N'Code', N'IsActive', N'Name') AND [object_id] = OBJECT_ID(N'[Countries]'))
+        SET IDENTITY_INSERT [Countries] ON;
+    EXEC(N'INSERT INTO [Countries] ([Id], [BaseShippingRate], [Code], [IsActive], [Name])
+    VALUES (1, 5.0, N''IN'', CAST(1 AS bit), N''India''),
+    (2, 8.0, N''US'', CAST(1 AS bit), N''Estados Unidos''),
+    (3, 10.0, N''UK'', CAST(1 AS bit), N''Reino Unido'')');
+    IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'BaseShippingRate', N'Code', N'IsActive', N'Name') AND [object_id] = OBJECT_ID(N'[Countries]'))
+        SET IDENTITY_INSERT [Countries] OFF;
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260421055732_InitialCreate'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20260421055732_InitialCreate', N'9.0.0');
+END;
+
+COMMIT;
+GO
+
